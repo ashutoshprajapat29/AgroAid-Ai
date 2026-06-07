57f1d5220f9f03c70281d74bb1347e102964f9ce
Ashutosh Prajapat <ashutoshprajapat2932005@gmail.com>
2026-06-07 14:42:59 +0530

    FIX:Sort the prices of commodities based on date

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.p49q8jsoiqo"
+    "revision": "0.e8sb3h9p2tg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
